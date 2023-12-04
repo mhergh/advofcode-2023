dddba79fdfab030a024f5ed93dd43d6be9135200
mhergh <66273120+mhergh@users.noreply.github.com>
2023-12-04 21:24:39 +0100

    dropped debug 20231201_2half_solution.pl

--- 20231201_2half_solution.pl
+++ 20231201_2half_solution.pl
@@ -2,8 +2,7 @@
 
 BEGIN{
   %m=("zero"=>0,"one"=>1,"two"=>2,"three"=>3,"four"=>4,"five"=>5,"six"=>6,"seven"=>7,"eight"=>8,"nine"=>9,);
-  $reDigit=join("|", sort(keys(%m)));
-  print "regex digit part:<$reDigit>"
+  $reDigit=join("|", sort(keys(%m)))
 }
 
 END{
